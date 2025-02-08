26b48ef33f82f678b328e479adcd8870dd230d91
iCHRI77 <christianjbarbosab@gmail.com>
2025-02-08 18:58:57 -0500

    feature($demo_video): add iframe of youtube with the demo

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="mt-5 text-justify w-1/2 mx-auto">
           This is an app developed in <a href="https://nextjs.org/" target="_blank" className="text-cyan-500 underline">Next.js</a> with
           API integrations. Here we are integrating the <a href="https://redux-toolkit.js.org/" target="_blank" className="text-cyan-500 underline">React Redux.js</a> library,
-          the <a href="https://open-meteo.com/" target="_blank" className="text-cyan-500 underline">Open Meteo API</a> to get weather data with <a href="https://react-leaflet.js.org/" target="_blank" className="text-cyan-500 underline">React Leaftlet 5.x</a> for the map library, <a href="https://react-icons.github.io/react-icons/" target="_blank" className="text-cyan-500 underline">React Icons</a> library for the icons and some animations with <a href="https://motion.dev/" target="_blank" className="text-cyan-500 underline">Motion</a> library.
+          the <a href="https://www.openstreetmap.org/" target="_blank" className="text-cyan-500 underline">Open Street Maps</a>, to get the Cities by lat and long atributes, the <a href="https://open-meteo.com/" target="_blank" className="text-cyan-500 underline">Open Meteo API</a> to get weather data with <a href="https://react-leaflet.js.org/" target="_blank" className="text-cyan-500 underline">React Leaftlet 5.x</a> for the map library, <a href="https://react-icons.github.io/react-icons/" target="_blank" className="text-cyan-500 underline">React Icons</a> library for the icons and some animations with <a href="https://motion.dev/" target="_blank" className="text-cyan-500 underline">Motion</a> library.
           For showing my skills integrating different libraries and doing interactive components, this is for you: Go to see it <Link href="/InAction" className="text-red-600 underline">In Action</Link>.
         </p>
       </section>
@@ -28,21 +28,17 @@ export default function Home() {
         }}
         className="relative mt-5 justify-self-center overflow-hidden"
       >
-        {/* <iframe
-          width={720}
-          height={360}
-          src="https://www.youtube.com/embed/ZjAqacIC_3c?list=PLC3y8-rFHvwjOKd6gdf4QtV1uYNiQnruI"
-          title="Next.js 14 Tutorial - 1 - Introduction"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen>
-        </iframe> */}
-        <div>
-          <p><i>Video Here</i></p>
+        <div style={{paddingBottom:'50px'}}>
+          <iframe
+            width={720}
+            height={360}
+            src="https://www.youtube.com/embed/iL2qBsmi3-g" title="Next.js - How I integrate Leaftlet 5.x with Open Meteo API and Open Street Map"
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen>
+          </iframe>
         </div>
-
-
       </motion.div>
     </div>
   );
